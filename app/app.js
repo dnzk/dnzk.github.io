7361ba54179c71793d6b94d40e606d9c18404f59
danish <danish.kalesaran@invoice2go.com>
2015-10-10 01:29:30 +0700

    Test angular js

---
+++ app/app.js
@@ -0,0 +1,11 @@
+(function(angular) {
+  'use strict';
+
+  angular.module('swapi', [])
+    .controller('BodyController', function($scope) {
+      function initialize() {
+        console.log('chthulu dawn');
+      }
+      initialize();
+    });
+})(angular);
