0669d89b77c6d707b5b27c7567d3a96313d1c420
danish <danish.kalesaran@invoice2go.com>
2016-02-27 22:04:27 +0700

    initialize bower package; add d3.js as dependency; create main svg element in script.js to test d3;

---
+++ script.js
@@ -0,0 +1,16 @@
+(function() {
+
+  'use strict'
+
+  var svgWidth = 500;
+  var svgHeight = 500;
+
+  // declare main svg
+  var svg = d3.select('body')
+    .append('svg')
+      .attr({
+        'width': 500,
+        'height': 500
+      });
+
+})();
