89669d33367e42877d1276adedeb1e86ec7170b6
danish <danish.kalesaran@invoice2go.com>
2016-02-28 16:31:36 +0700

    draw grey background on svg

--- script.js
+++ script.js
@@ -3,14 +3,25 @@
   'use strict'
 
   var svgWidth = 500;
-  var svgHeight = 500;
+  var svgHeight = 200;
+
+  var colorScheme = {
+    background: '#CCC'
+  };
 
   // declare main svg
   var svg = d3.select('body')
     .append('svg')
       .attr({
-        'width': 500,
-        'height': 500
+        'width': svgWidth,
+        'height': svgHeight
       });
+
+  var svgBackground = svg.append('rect')
+    .attr({
+      'width': '100%',
+      'height': '100%',
+      'fill': colorScheme.background
+    });
 
 })();
